d1742c4f0e3ab81b51803cf6f93571114b77d982
JoeyByrd <joeybyrd9999@gmail.com>
2022-05-19 01:32:47 -0700

    added some javascript

---
+++ assets/questions.js
@@ -0,0 +1,33 @@
+// list of all questions, answers, and choices
+var questions = [
+    {
+       title: "",
+       choices: "",
+       answer: "" 
+    },
+    {
+        title: "",
+        choices: "",
+        answer: "" 
+     },
+     {
+        title: "",
+        choices: "",
+        answer: "" 
+     },
+     {
+        title: "",
+        choices: "",
+        answer: "" 
+     },
+     {
+        title: "",
+        choices: "",
+        answer: "" 
+     },
+     {
+        title: "",
+        choices: "",
+        answer: "" 
+     },
+];
---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// varibales
+
+//Start quiz button
+
+//6 questions possibly randomized
+
+//60 second timer
+
+//local storage
